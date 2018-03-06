e8350fc4769fcfe383edccab99633b3ee6789b55
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-03-05 19:19:08 -0500

    iris replies on slack

--- bin/run.js
+++ bin/run.js
@@ -1,16 +1,36 @@
 'use strict';
 
+const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
 
+const token = 'xoxb-324793832340-g8ymOP51YsLeM3WFkpjWFDog';
+const slackLogLevel='verbose';
+
+// service has express object and it is passed to createServer method
+// this helps in creating an express server
 const server = http.createServer(service);
-server.listen(3000);
+
+/* pass the token and loglevel to slack client
+the slack client is an API token for the slack app which has a bot user added to it
+Using this slack client we will be able to chat with the bot
+*/
+const rtm = slackClient.init(token,slackLogLevel);
+
+//after doing this you can see a green icon on iris the slack bot
+rtm.start();
+
+
+
+slackClient.addAuthenticatedHandler(rtm, ()=> server.listen(3000));
+//server is created and is listening on port 3000
+
 console.log('here');
 
 
 console.log('server started');
 
-
+//log if the server is started successfully
 server.on('listening', function() {
     console.log(`server is listening on${server.address().port}`);
 });
--- server/slackClient.js
+++ server/slackClient.js
@@ -1,32 +1,77 @@
-const { RtmClient, CLIENT_EVENTS } = require('@slack/client');
+'use strict';
+
+const { RtmClient, CLIENT_EVENTS, RTM_EVENTS } = require('@slack/client');
+
+let rtm = null;
+
 
 // An access token (from your Slack app or custom integration - usually xoxb)
-const token = process.env.SLACK_TOKEN;
-const token = 'xoxb-324793832340-sFFzXjiXiXRpLM2q0DLUrldJ';
+// const token = process.env.SLACK_TOKEN;
+// const token = 'xoxb-324793832340-sFFzXjiXiXRpLM2q0DLUrldJ';
 
-// Cache of data
-const appData = {};
+
+function handleOnAuthenticated(rtmStartData) {
+    console.log(`Logges in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+// it takes as input real time messaging and event handler as input 
+function addAuthenticatedHandler(rtm, handler) {
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+
+}
+
+
+function handleOnMessage(message) {
+console.log(message);
+rtm.sendMessage('Hello, world!', message.channel, function messageSent(){
+    //optional
+    console.log('message sent');
+})
+}
+
+module.exports.init = function slackClient(token, slackLogLevel){
 
 // Initialize the RTM client with the recommended settings. Using the defaults for these
 // settings is deprecated.
-const rtm = new RtmClient(token, {
-  dataStore: false,
-  useRtmConnect: true,
-});
+/* the loglevel is a logger provided by stackClient which does by default logging for RTM
+   the loglevel types are default and verbose
+    here using the Rt
+*/
 
-// The client will emit an RTM.AUTHENTICATED event on when the connection data is available
-// (before the connection is open)
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (connectData) => {
-  // Cache the data necessary for this app in memory
-  appData.selfId = connectData.self.id;
-  console.log(`Logged in as ${appData.selfId} of team ${connectData.team.id}`);
-});
+ rtm = new RtmClient(token, {logLevel : slackLogLevel}, {
+    dataStore: false,
+    useRtmConnect: true,
+  });
 
-// The client will emit an RTM.RTM_CONNECTION_OPENED the connection is ready for
-// sending and receiving messages
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-  console.log(`Ready`);
-});
+  
+  addAuthenticatedHandler(rtm, handleOnAuthenticated);
+  rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
 
-// Start the connecting process
-rtm.start();
+  return rtm;
+
+}
+
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
+    
+
+// // Cache of data
+// const appData = {};
+
+
+
+// // The client will emit an RTM.AUTHENTICATED event on when the connection data is available
+// // (before the connection is open)
+// rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (connectData) => {
+//   // Cache the data necessary for this app in memory
+//   appData.selfId = connectData.self.id;
+//   console.log(`Logged in as ${appData.selfId} of team ${connectData.team.id}`);
+// });
+
+// // The client will emit an RTM.RTM_CONNECTION_OPENED the connection is ready for
+// // sending and receiving messages
+// rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+//   console.log(`Ready`);
+// });
+
+// // Start the connecting process
+// rtm.start();
