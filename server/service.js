0cc737835905b8967a5ddc163de4cd1611b25328
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-03-08 13:00:30 -0500

    backup time working

--- server/service.js
+++ server/service.js
@@ -4,6 +4,21 @@ const express = require('express');
 const service = express();
 
 
+
+service.get('/service/:intent/:port', (req, res, next) => {
+    const serviceIntent = req.params.intent;
+    const servicePort = req.params.port;
+
+    // if IPV6 double 
+    const serviceIp = req.connection.remoteAddress.includes('::')
+    ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+
+    res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`});
+
+
+});
+
 module.exports = service;
 
 // b82864149b5cda1a4de2bee712e970bd
